06f8725d4b08857b1fefa208456c052a09abde2e
Edwardmamani <mamanizapanaedward@gmail.com>
2024-08-28 21:01:03 -0500

    formulario

--- app/src/main/java/edward/mamani/matriculacrud/MainActivity.java
+++ app/src/main/java/edward/mamani/matriculacrud/MainActivity.java
@@ -1,15 +1,10 @@
 package edward.mamani.matriculacrud;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import edward.mamani.matriculacrud.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,18 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Inflar el diseño y obtener el binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-    }
+        // Configurar un listener para un botón
+        binding.myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mostrar un mensaje cuando el botón es clickeado
+                Toast.makeText(MainActivity.this, "Botón clickeado", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        // Configurar un TextView
+        binding.myTextView.setText("Texto actualizado desde el código");
+    }
 }
