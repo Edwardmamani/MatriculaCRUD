a7d8f3a7874ea1a2c6629f461b8abbb3a5d663fc
Edwardmamani <mamanizapanaedward@gmail.com>
2024-08-28 22:06:38 -0500

    administradacion de db

---
+++ app/src/main/java/edward/mamani/matriculacrud/AlumnoActivity.java
@@ -0,0 +1,35 @@
+package edward.mamani.matriculacrud;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import edward.mamani.matriculacrud.databinding.ActivityAlumnoBinding;
+import edward.mamani.matriculacrud.databinding.ActivityMainBinding;
+
+public class AlumnoActivity extends AppCompatActivity {
+
+    private ActivityAlumnoBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Inflar el diseño y obtener el binding
+        binding = ActivityAlumnoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_alumno);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
--- app/src/main/java/edward/mamani/matriculacrud/MainActivity.java
+++ app/src/main/java/edward/mamani/matriculacrud/MainActivity.java
@@ -1,8 +1,6 @@
 package edward.mamani.matriculacrud;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import edward.mamani.matriculacrud.databinding.ActivityMainBinding;
@@ -14,21 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Inflar el diseño y obtener el binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        // Configurar un listener para un botón
-        binding.myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Mostrar un mensaje cuando el botón es clickeado
-                Toast.makeText(MainActivity.this, "Botón clickeado", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // Configurar un TextView
-        binding.myTextView.setText("Texto actualizado desde el código");
     }
 }
